f6969faec6fb999aa9208f119cfdc7dfa2f088f2
santosh-rajan <110458992+santosh-rajan@users.noreply.github.com>
2023-08-03 14:51:07 +0530

    update to Bigquery

--- nukds/_modidx.py
+++ nukds/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://santosh-rajan.github.io',
                 'git_url': 'https://github.com/santosh-rajan/nukds',
                 'lib_path': 'nukds'},
-  'syms': {'nukds.core': {'nukds.core.foo': ('core.html#foo', 'nukds/core.py')}}}
+  'syms': { 'nukds.bigquery': { 'nukds.bigquery.create_client': ('bigquery.html#create_client', 'nukds/bigquery.py'),
+                                'nukds.bigquery.query_to_dataframe': ('bigquery.html#query_to_dataframe', 'nukds/bigquery.py')},
+            'nukds.core': {'nukds.core.foo': ('core.html#foo', 'nukds/core.py')}}}
